857397a6061921d3ee867daf57282640d097a0f1
Pavithran V <79012930+its-pavithran-v@users.noreply.github.com>
2021-06-06 20:33:17 +0530

    validates more urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,25 @@
 from flask import Flask, render_template, request, url_for
 import jinja2
+import re
 from urllib.parse import urlparse, parse_qs
 app = Flask(__name__)
 
 @app.route('/', methods=["POST","GET"])
-def homepage():
 
+def homepage():
     if request.method == "GET":
         return render_template('index.html')
-
     if request.method == "POST":
         url = request.form["url"]
         yt_url = urlparse(url)
-        if yt_url.hostname == 'youtu.be':
-            video_id = yt_url.path[1:]
-            return render_template('index.html', youtube_id = video_id)
-        if yt_url.hostname in {'www.youtube.com', 'youtube.com'}:
-            if yt_url.path == '/watch':
-                video_id = parse_qs(yt_url.query)['v'][0]
-            elif yt_url.path[:7] == '/watch/':
-                video_id = yt_url.path.split('/')[1]
-            elif yt_url.path[:7] == '/embed/':
-                video_id = yt_url.path.split('/')[2]
-            elif yt_url.path[:3] == '/v/':
-                video_id = yt_url.path.split('/')[2]
-            elif yt_url.path[:9] == '/playlist':
-                video_id = parse_qs(yt_url.query)['list'][0]
-                return render_template('index.html', youtube_id = video_id , playlist = True)
-            else:
-                video_id = yt_url.path.split('/')[1]
-            return render_template('index.html', youtube_id = video_id)
-        else:
-            return render_template('index.html')
+        
+        if yt_url.path[:9] == '/playlist':
+                p_id = parse_qs(yt_url.query)['list'][0]
+                return render_template('index.html', playlist_id = p_id)
+        
+        idRegex = re.compile(r'(?:\/|%3D|v=|vi=)([0-9A-z-_]{11})(?:[%#?&]|$)')
+        v_id = idRegex.search(url)
+        return render_template('index.html', youtube_id = v_id.group(1))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
